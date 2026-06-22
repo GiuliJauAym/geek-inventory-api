da7d967ca0003ce7a50ddf1f1d18e5f62f4809ff
GiuliJauAym <giuli.jau.aym@gmail.com>
2026-06-21 21:51:22 -0300

    Usar puerto 1433 sin instanceName en Sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
+  port: 1433,
   dialect: 'mssql',
   logging: process.env.NODE_ENV === 'development' ? console.log : false,
   define: {
@@ -22,7 +23,6 @@ const sequelize = new Sequelize({
     options: {
       encrypt: false,
       trustServerCertificate: true,
-      instanceName: 'SQLEXPRESS',
       connectTimeout: 30000,
     },
   },
