23d88f4634b5ffd276978508b7be9bf31357175a
GiuliJauAym <giuli.jau.aym@gmail.com>
2026-06-21 22:07:21 -0300

    database.js

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,6 @@ const sequelize = new Sequelize({
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
   dialect: 'mssql',
   logging: process.env.NODE_ENV === 'development' ? console.log : false,
   define: {
